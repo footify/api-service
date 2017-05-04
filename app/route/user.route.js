85ac253e7ab0a5e5c11e7ed4de90fd12c6a6260c
Maxime Guedj <maxime@guedjdev.com>
2017-05-04 20:34:35 +0000

    Integrate feed user

--- app/route/user.route.js
+++ app/route/user.route.js
@@ -1,6 +1,7 @@
 const boom = require('boom');
 const passport = require('passport');
 const schemas = require('./user.schema');
+const matchSchema = require('./match.schema');
 const dataApi = require('@footify/data-api');
 const httpHelper = require('@footify/http-helper');
 
@@ -8,6 +9,7 @@ function registerRoute(router) {
     router.get('/users/me', passport.authenticate('bearer', { session : false }), httpHelper.generateRoute(getCurrentUserInfo));
     router.get('/users/:id', passport.authenticate('basic', { session: false }), httpHelper.generateRoute(getUserInformation));
     router.get('/users/me/friends', passport.authenticate('bearer', { session : false }), httpHelper.generateRoute(getUserFriend));
+    router.get('/users/me/feed', passport.authenticate('bearer', { session : false }), httpHelper.generateRoute(getUserFeed));
     router.post('/users/me/friends/invite/:id', passport.authenticate('bearer', { session : false }), httpHelper.generateRoute(sendInvitation));
     router.post('/users/me/friends/accept/:id', passport.authenticate('bearer', { session : false }), httpHelper.generateRoute(acceptInvitation));
     router.post('/users/me/friends/deny/:id', passport.authenticate('bearer', { session : false }), httpHelper.generateRoute(denyInvitation));
@@ -104,6 +106,22 @@ function denyInvitation(req, res, next) {
                 });
         }).catch((e) => {
             httpHelper.handleError(res, e);
+        });
+}
+
+function getUserFeed(req, res, next) {
+    return dataApi.gameRepository.getAllGamesByUserId(req.user._id)
+        .then((games) => {
+            let output = [];
+            for (let game of games) {
+                game = game.toObject();
+                let teams = game.teams;
+                game.teams = [];
+                game.teams.push({ id: teams[0]._id, players: [httpHelper.utils.toSnakeCase(teams[0].player1), httpHelper.utils.toSnakeCase(teams[0].player2)]});
+                game.teams.push({ id: teams[1]._id, players: [httpHelper.utils.toSnakeCase(teams[1].player1), httpHelper.utils.toSnakeCase(teams[1].player2)]});
+                output.push(game);
+            }
+            httpHelper.sendReply(res, 200, output, matchSchema.matchFeedSchema);
         });
 }
 
